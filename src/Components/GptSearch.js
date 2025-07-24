5565393d8a4db1932ebe5b240a83ba44894dd4f0
@ranamuzahid <ranamuzahid@webzent.in>
2025-07-24 10:53:15 +0530

    multi language changes

---
+++ src/Components/GptSearch.js
@@ -0,0 +1,19 @@
+import GptSearchBar from "./GptSearchBar";
+import GptMovieSuggestion from "./GptMovieSuggestion";
+import { BG_URL } from "../utils/constant";
+
+const GptSearch = () => {
+  return (
+    <div>
+      <div className="absolute -z-10">
+        <img src={BG_URL} alt="LOGO" />
+      </div>
+      <div>
+        <GptSearchBar />
+        <GptMovieSuggestion />
+      </div>
+    </div>
+  );
+};
+
+export default GptSearch;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,7 +9,7 @@ import {
 import { useDispatch } from "react-redux";
 import { auth } from "../utils/firebase";
 import { addUser } from "../utils/userSlice";
-import { USER_AVTAR } from "../utils/constant";
+import { BG_URL, USER_AVTAR } from "../utils/constant";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -86,10 +86,7 @@ const Login = () => {
     <>
       <Header />
       <div className="absolute">
-        <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-          alt="bg-img"
-        />
+        <img src={BG_URL} alt="bg-img" />
       </div>
       <form
         onSubmit={(e) => {
